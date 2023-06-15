2bcc06e533780fc3ad6c89a7a2b56f254db2c39e
MINA-sayed <menaboss13@gmail.com>
2023-06-15 13:11:23 +0000

    updated models and controllers

--- src/models/dispenser.js
+++ src/models/dispenser.js
@@ -1,18 +1,27 @@
 const mongoose = require('mongoose');
 
 const dispenserSchema = new mongoose.Schema({
-    id: { uuid: true, default: mongoose.Types.ObjectId() },
-    amount: { type: Number },
-    usages: [{
-        opened_at: { type: Date, default: Date.now() },
-        closed_at: { type: Date, default: Date.now() },
-        flow_volume: { type: Number },
-        total_spent: { type: Number },
-    }]
-   
+  uuid: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+  },
+  usages: [
+    {
+      opened_at: { type: Date, default: Date.now() },
+      closed_at: { type: Date, default: Date.now() },
+      flow_volume: { type: Number },
+      total_spent: { type: Number },
+    },
+  ],
+});
+
+dispenserSchema.virtual('id').get(function () {
+  return this.uuid + '-' + this.amount;
 });
 
 const Dispenser = mongoose.model('Dispenser', dispenserSchema);
-
 
 module.exports = Dispenser;
